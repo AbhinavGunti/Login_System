5a521d20d31612007bc6426b29e2c7a3bc97f0ee
AbhinavGunti <78372006+AbhinavGunti@users.noreply.github.com>
2022-06-27 13:18:40 +0530

    Register page working

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app= express();
+const bcrypt = require('bcrypt')
+const users = [];
 
 app.set('view-engine','ejs')
 
+app.use(express.urlencoded({extended:false}))
+
 app.get('/',(req,res)=>{
-    res.render('index.ejs') //render index.ejs file 
+    res.render('index.ejs',{name : 'xyz'}) //render index.ejs file 
 })
 
+app.get('/login',(req,res)=>{
+    res.render('login.ejs')
+})
+app.get('/register',(req,res)=>{
+    res.render('register.ejs')
+})
+app.post('/login',(req,res)=>{
+    
+})
+app.post('/register',(req,res)=>{
+    try{
+        const hashedPassword = bcrypt.hash(req.body.password,10)
+        users.push({
+            id: Date.now().toString(),
+            name:req.body.name,
+            email:req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')  //on success redirect to login page
+    }catch{
+        res.redirect('/register')   //if err,redirect them to redirect page again
+    }
+})
 app.listen(3000)
